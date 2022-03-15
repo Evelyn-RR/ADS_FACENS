91589fdbd1ce71b40a6f149c2fa6b7de94d8c9cf
Evelyn-RR <vilyn.rodrigues8901@gmail.com>
2022-03-14 22:28:03 -0300

    Aula1

---
+++ Programacao_para_dispositiveis_moveis/app/src/main/java/com/example/aula2/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.aula2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    AutoCompleteTextView autocompleteCurso;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        autocompleteCurso =
+                (AutoCompleteTextView) findViewById(R.id.eeditTextCurso);
+        autocompleteCurso.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                boolean retorno = false;
+                if (actionId == EditorInfo.IME_ACTION_SEND){
+                    buttonSalvarClick(null);
+                    retorno = true;
+                }
+                return retorno;
+            }
+        });
+
+        String[] cursos = getResources().getStringArray(R.array.cursos_array);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, cursos);
+        autocompleteCurso.setAdapter(adapter);
+
+    }
+
+    public void buttonSalvarClick(View view){
+        Toast.makeText(this, "Salvo com sucesso", Toast.LENGTH_SHORT).show();
+    }
+}
