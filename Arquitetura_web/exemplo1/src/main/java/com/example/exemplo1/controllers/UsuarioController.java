0c8d4ef07f38aefe657a308a94c278ce61d5189b
Evelyn-RR <vilyn.rodrigues8901@gmail.com>
2022-03-14 21:47:18 -0300

    Exercicio1

---
+++ Arquitetura_web/exemplo1/src/main/java/com/example/exemplo1/controllers/UsuarioController.java
@@ -0,0 +1,31 @@
+package com.example.exemplo1.controllers;
+
+import java.util.ArrayList;
+
+import com.example.exemplo1.Models.Usuario;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/Usuario")
+
+public class UsuarioController {
+    static ArrayList<Usuario> listaUsuario = new ArrayList<>();
+
+    @GetMapping()
+    public ArrayList<Usuario> getUsuarios() {
+        return listaUsuario;
+    }
+
+    @PostMapping()
+    public ArrayList<Usuario> postUsuario(@RequestBody Usuario entity) {
+        listaUsuario.add(entity);        
+        return listaUsuario;
+    }
+    
+    
+}
