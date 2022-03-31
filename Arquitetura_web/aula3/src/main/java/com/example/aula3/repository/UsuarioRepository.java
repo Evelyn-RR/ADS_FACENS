b7aa4255483ce49a83c9a844fa0acfefcfbbcbe3
Evelyn-RR <vilyn.rodrigues8901@gmail.com>
2022-03-30 22:27:52 -0300

    Aula4-Exercicios

--- Arquitetura_web/aula3/src/main/java/com/example/aula3/repository/UsuarioRepository.java
+++ Arquitetura_web/aula3/src/main/java/com/example/aula3/repository/UsuarioRepository.java
@@ -16,8 +16,8 @@ import org.springframework.stereotype.Repository;
 public class UsuarioRepository {
     private static String INSET = "insert into usuario(nome,email,senha) values(?,?,?)";
     private static String SELECT_ALL = "select * from usuario";
-    //private static String DELETE = "delete from usuario WHERE id = ?";
-    //private static String EDIT = "edit into usuario(nome,email,senha) values(?,?,?)";
+    private static String DELETE = "delete from usuario WHERE id = ?";
+    private static String EDIT = "update usuario set nome = admin, email = admin@admin.com, senha = 321 where id = 1";
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -27,6 +27,24 @@ public class UsuarioRepository {
         jdbcTemplate.update(INSET, new Object[]
             {usuario.getNome(), usuario.getEmail(), usuario.getSenha()});
         return usuario;
+    }
+
+    public void delete(int id){
+        jdbcTemplate.update(DELETE, id);
+    }
+
+    public Usuario delete(Usuario usuario){
+        delete(jdbcTemplate.find(usuario, id));
+    }
+
+    public Usuario edit(Usuario usuario){
+        this.jdbcTemplate.update(EDIT, new Object[]
+            {usuario.getNome(), usuario.getEmail(), usuario.getSenha()});
+        return usuario;
+    }
+
+    public boolean(String email, String senha){
+
     }
 
     public List<Usuario> obterTodos(){
